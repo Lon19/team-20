7a05976366e54fa8371dbfade98d6e8cf004e45c
William Burns <william@williamburns.co.uk>
2019-11-02 08:49:26 +0000

    mapbox end me

--- logic.js
+++ logic.js
@@ -1,5 +1,6 @@
 const densities = [860, 260, 145, 105, 50, 20]; // higher -> lower
-const colors = ['#ff0000', '#ffa600', '#fcbe03', '#ffff00', '#80ff00', '#00ff1e'];
+// const colors = ['#ff0000', '#ffa600', '#fcbe03', '#ffff00', '#80ff00', '#00ff1e'];
+const colors = ['#f24343', '#f27171', '#f2a371', '#f2d671', '#dcf271', '#85f271'];
 
 const simpleOne = 'Wards_December_2015_Full_Clip-bxp05f';
 const detailedOne = 'Wards_December_2015_Full_Clip-41pq8l';
@@ -7,9 +8,11 @@ const detailedOne = 'Wards_December_2015_Full_Clip-41pq8l';
 let data;
 let oldZoom;
 
+let detailedLayer = false;
+
 window.onload = function(){
 
-    fetch('data.json').then(resp => {
+    fetch('data_processed_added.json').then(resp => {
         resp.json().then(d => {
             data = d;
         });
@@ -17,84 +20,83 @@ window.onload = function(){
 
 };
 
+var popup = new mapboxgl.Popup({
+    closeButton: false,
+    closeOnClick: false
+});
+
+map.on('mouseenter', 'simpleone', function(f){
+    map.getCanvas().style.cursor = 'pointer';
+
+    popup.setLngLat([f.lngLat.lng, f.lngLat.lat]).setHTML('pop').addTo(map);
+});
+
+map.on('mouseleave', 'simpleone', function(f){
+    map.getCanvas().style.cursor = '';
+    popup.remove();
+});
+
 map.on('load', function(e){
 
     for (let i = 0; i < densities.length; i++){
         map.addLayer({
-            id: 'density' + densities[i],
+            id: 'density' + densities[i] + 's',
             source: 'composite',
             'source-layer': simpleOne,
             type: 'fill',
             paint: {
-                'fill-color': colors[i]
+                'fill-color': colors[i],
+                'fill-outline-color': '#ffffff'
             }
         });
+
+        // map.on('mouseenter', 'density' + densities[i], function(f){
+        //     map.getCanvas().style.cursor = 'pointer';
+        //
+        //     popup.setLngLat([f.lngLat.lng, f.lngLat.lat]).setHTML('pop').addTo(map);
+        // });
+        //
+        // map.on('mouseleave', 'density' + densities[i], function(f){
+        //     map.getCanvas().style.cursor = '';
+        //     popup.remove();
+        // });
     }
+
+    update(simpleOne, 's');
 
     oldZoom = map.getZoom();
 });
 
 map.on('zoom', function(e){
-    console.log("did zoom. oldZoom was " + oldZoom + " and now is " + map.getZoom());
     if (map.getZoom() < 7 && oldZoom >= 7){
-        update(simpleOne);
-    } else if (map.getZoom > 7 && oldZoom <= 7) {
-        update(detailedOne);
+        update(simpleOne, 's');
+        console.log("switching to simple");
+    } else if (map.getZoom() > 7 && oldZoom <= 7) {
+        update(detailedOne, 'd');
+        console.log("switching to detailed");
     }
 
     oldZoom = map.getZoom();
 });
 
-map.on('load', function(e){
+function update(type, character){
+    if (!detailedLayer){
+        for (let i = 0; i < densities.length; i++){
+            map.addLayer({
+                id: 'density' + densities[i] + 'd',
+                source: 'composite',
+                'source-layer': detailedOne,
+                type: 'fill',
+                paint: {
+                    'fill-color': colors[i],
+                    'fill-outline-color': '#ffffff'
+                }
+            });
+        }
 
+        detailedLayer = true;
+    }
 
-    // for (let j = 0; j < sf.length; j++){
-    //     let name = sf[j].properties.wd11nm;
-    //
-    //     if (!data[name]){
-    //         continue;
-    //     }
-    //
-    //
-    //     if (!sf[j].properties.density){
-    //         // sf[j].properties.density = getMatchingDensity(data[name]);
-    //
-    //         // map.setFeatureState({
-    //         //     id: sf[j].id,
-    //         //     source: 'composite',
-    //         //     sourceLayer: 'Wards_December_2011_Boundarie-4lgzg3'
-    //         // }, {
-    //         //     properties: {
-    //         //         density: getMatchingDensity(data[name])
-    //         //     }
-    //         // });
-    //
-    //         //Draw.setFeatureProperty(sf[j].id, 'density', getMatchingDensity(data[name]));
-    //
-    //         // map.setFilter('density' + getMatchingDensity(data[name]), ['==', 'density', getMatchingDensity(data[name])]);
-    //     }
-    // }
-
-    // if (!done) {
-    //     map.addLayer({
-    //         id: 'some-new-layer',
-    //         source: 'composite',
-    //         'source-layer': 'Wards_December_2011_Boundarie-4lgzg3',
-    //         type: 'fill',
-    //         paint: { 'fill-color': '#ffffff' }
-    //     });
-    //
-    //     map.setFilter('some-new-layer', ['==', 'wd11nm', 'Gotham']);
-    //
-    //     done = true;
-    // }
-
-    // for (i = 0; i < sf.length; i++){
-    //     sf[i]
-    // }
-});
-
-function update(type){
     var sf = map.querySourceFeatures('composite', {sourceLayer:
         type});
 
@@ -103,7 +105,7 @@ function update(type){
     }
 
     for (let d = 0; d < densities.length; d++){
-        map.setFilter('density' + densities[d], null); // clear filters
+        map.setFilter('density' + densities[d] + character, null); // clear filters
 
         var filters = ["any"];
 
@@ -119,7 +121,7 @@ function update(type){
             }
         }
 
-        map.setFilter('density' + densities[d], filters)
+        map.setFilter('density' + densities[d] + character, filters);
     }
 }
 
