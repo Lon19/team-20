722db3bf97b1a7248b1908b35c9b5512d8992db3
William Burns <william@williamburns.co.uk>
2019-11-02 01:26:15 +0000

    Density to colour function.

--- logic.js
+++ logic.js
@@ -25,6 +25,16 @@ map.on('zoom', function(e){
 
 function densityToColor(density){
 
+    const densities = [860, 260, 145, 105, 50, 20]; // higher -> lower
+    const colors = ['#ff0000', '#ffa600', '#ffff00', '#80ff00', '#00ff1e'];
 
+    let ret;
 
+    for (let i = 0; i < densities.length; i++){
+        if (density <= densities[i]){
+            ret = colors[i];
+        }
+    }
+
+    return ret;
 }
